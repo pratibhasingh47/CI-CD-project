b316f026c53eb2efbe96a9bdf021e0dadc3baf20
PRATIBHA SINGH <117928965+pratibhasingh47@users.noreply.github.com>
2025-03-29 12:18:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:$BUILD_ID
+docker tag $JOB_NAME:$BUILD_ID pratibhasingh47/$JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:latest
+docker tag $JOB_NAME:$BUILD_ID pratibhasingh47/$JOB_NAME:latest
 
-docker push vaibhavjoshi26/$JOB_NAME:$BUILD_ID
+docker push pratibhasingh47/$JOB_NAME:$BUILD_ID
 
-docker push vaibhavjoshi26/$JOB_NAME:latest
+docker push pratibhasingh47/$JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:latest
+docker rmi -f $JOB_NAME:$BUILD_ID pratibhasingh47/$JOB_NAME:$BUILD_ID pratibhasingh47/$JOB_NAME:latest
